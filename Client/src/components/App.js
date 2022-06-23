e589024596a283b28d2b6ecda0ec673ff1dc2a04
Andrei Pana <andreimpana@gmail.com>
2022-06-23 18:12:59 -0400

    Deleting Listings working

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import Header from './Header';
-import {Router, Route, Switch} from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import ListingsList from './Listings/ListingsList';
 import history from '../history';
 import ListingsCreate from './Listings/ListingsCreate';
 import ListingContact from './Listings/ListingContact';
 import About from './About';
+import ListingsDelete from './Listings/ListingsDelete';
 
-class App extends React.Component{
-    render(){
+class App extends React.Component {
+    render() {
         return (
             <div>
                 <Router history={history}>
                     <Header></Header>
-                        <div className='ui container'>
-                            <div>
-                                <Switch>
-                                    <Route path="/" exact component={ListingsList}></Route>
-                                    <Route path="/createAd" exact component={ListingsCreate}></Route>
-                                    <Route path="/user/register" exact component={ListingsCreate}></Route>
-                                    <Route path="/user/login" exact component={ListingsCreate}></Route>
-                                    <Route path="/about" exact component={About}></Route>
-                                    <Route path="/contact/:id" exact component={ListingContact}></Route>
-                                </Switch>
-                            </div>
+                    <div className='ui container'>
+                        <div>
+                            <Switch>
+                                <Route path="/" exact component={ListingsList}></Route>
+                                <Route path="/createAd" exact component={ListingsCreate}></Route>
+                                <Route path="/user/register" exact component={ListingsCreate}></Route>
+                                <Route path="/user/login" exact component={ListingsCreate}></Route>
+                                <Route path="/about" exact component={About}></Route>
+                                <Route path="/contact/:id" exact component={ListingContact}></Route>
+                                <Route path="/delete/:id" exact component={ListingsDelete}></Route>
+                            </Switch>
                         </div>
+                    </div>
                 </Router>
-            </div> 
+            </div>
         );
     }
 };
--- Client/src/components/Listings/ListingsDelete.js
+++ Client/src/components/Listings/ListingsDelete.js
@@ -5,14 +5,43 @@ import { connect } from 'react-redux';
 import { fetchListing, deleteListing } from '../../actions';
 import { Link } from 'react-router-dom';
 
-class ListingsDelete extends Component {
+class ListingsDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchListing(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params
+    return (
+      //Use this when you want to return multiple elements and not have presence in the DOM or use <> </>
+      <React.Fragment>
+        <button onClick={() => this.props.deleteListing(id)} className='ui button negative'>Delete</button>
+        <Link to="/" className='ui button'>Cancel</Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.Listing) {
+      return "Are you sure you want to delete this listing?"
+    }
+    return `Are you sure you want to delete listing: ${this.props.Listing.title}`
+  }
+
   render() {
     return (
-      <div>
-        ListingsDelete
-      </div>
+      <Modal
+        title="Delete Listing"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
+      ></Modal>
     );
   }
 }
 
-export default ListingsDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { Listing: state.listings[ownProps.match.params.id] }
+};
+
+export default connect(mapStateToProps, { fetchListing, deleteListing })(ListingsDelete);
