49fa480959d7c47ddd8c46b172154dbdd64402f3
Andrei Pana <andreimpana@gmail.com>
2022-06-24 02:52:01 -0400

    css changes

--- Client/src/components/Listings/ListingsList.js
+++ Client/src/components/Listings/ListingsList.js
@@ -11,9 +11,9 @@ class ListingsList extends Component {
     renderList() {
         return this.props.listings.map(listing => {
             return (
-                <div className='card' key={listing.id} style={{ 'width': "366px" }}>
+                <div className='card' key={listing.id} style={{ 'width': "366px", "boxShadow": "1px 1px 4px" }}>
                     <div className='image'>
-                        <img src={listing.imageLink}></img>
+                        <img src={listing.imageLink} style={{ 'width': "366px", 'height': '275px' }}></img>
                     </div>
                     <div className="content">
                         <div className="header">{listing.title}</div>
@@ -21,8 +21,7 @@ class ListingsList extends Component {
                         <div className='description' style={{ 'fontSize': '26px', 'fontWeight': 'bold', 'color': 'green' }}>
                             {listing.price}
                             <span className='right floated'>
-                                {this.renderAdminButtons(listing)}
-                                <Link to={`/contact/${listing.id}`} className='ui green basic button'>Contact</Link>
+                                {this.renderControls(listing)}
                             </span>
                         </div>
                     </div>
@@ -31,11 +30,18 @@ class ListingsList extends Component {
         });
     }
 
-    renderAdminButtons(listing) {
+    renderControls(listing) {
         console.log(this.props.currentUserId)
         if (listing.userId === this.props.currentUserId) {
             return (
-                <Link to={`/delete/${listing.id}`} className='ui red button'>Delete</Link>
+                <div className='adminControls'>
+                    <Link to={`/delete/${listing.id}`} className='ui red button'>Delete</Link>
+                    <Link to={`/edit/${listing.id}`} className='ui blue inverted button'>Edit</Link>
+                </div>
+            );
+        } else {
+            return (
+                <Link to={`/contact/${listing.id}`} className='ui green inverted button'>Contact</Link>
             );
         }
     }
